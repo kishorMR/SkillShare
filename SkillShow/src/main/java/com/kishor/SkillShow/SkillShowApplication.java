976589fadeaa32d93324ae64d7e70a5f2dc1a01e
kishorMR <kishorgowda435@gmail.com>
2025-09-15 14:36:33 +0530

    Initial commit - Spring Boot project

---
+++ SkillShow/src/main/java/com/kishor/SkillShow/SkillShowApplication.java
@@ -0,0 +1,14 @@
+package com.kishor.SkillShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillShowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillShowApplication.class, args);
+		System.out.println("🚀 Starting Spring Boot Application...");
+	}
+
+}
